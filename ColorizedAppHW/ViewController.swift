6e31f68b847d2246422c2a4f9e1dc5c62eccda13
ZasyadkoAA <aleks.zasyadko@icloud.com>
2022-08-15 23:26:03 +0300

    Initial Commit

---
+++ ColorizedAppHW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedAppHW
+//
+//  Created by Александр Засядько on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
